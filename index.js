ac20faee2a5c5fa2f6eb262738c4baf3afba72af
Sergio Arias <s-andres140@hotmail.com>
2024-12-05 22:04:15 -0500

    feat: :hammer: update in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hola Mundo');
+    res.send('Hola Mundo, soy sergio');
 });
 
 app.listen(PORT, () => {
